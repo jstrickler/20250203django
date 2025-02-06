19643d316ef23af311fcd827955f3dd854eeba76
jstrickler <jstrickler@gmail.com>
2025-02-06 09:30:50 -0500

    end of wednesday

--- EXAMPLES/djangoexamples/djmodels/superheroes/models.py
+++ EXAMPLES/djangoexamples/djmodels/superheroes/models.py
@@ -1,36 +1,52 @@
+from uuid import uuid4
 from django.db import models
 
-
 class Power(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
     name = models.CharField(max_length=32, unique=True)
     description = models.CharField(max_length=512)
+
+    class Meta:
+        db_table = "powers"
 
     def __str__(self):
         return self.name
 
 
 class City(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
     name = models.CharField(max_length=32, unique=True)
+
+    class Meta:
+        db_table = "cities"
 
     def __str__(self):
         return self.name
 
 
 class Enemy(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
     name = models.CharField(max_length=32, unique=True)
     powers = models.ManyToManyField(Power)
+
+    class Meta:
+        db_table = "enemies"
 
     def __str__(self):
         return self.name
 
 
 class Superhero(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
     name = models.CharField(max_length=32, unique=True)
     real_name = models.CharField(max_length=32)
     secret_identity = models.CharField(max_length=32)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     powers = models.ManyToManyField(Power)
     enemies = models.ManyToManyField(Enemy)
+
+    class Meta:
+        db_table = "superheroes"
 
     def __str__(self):
         return self.name
